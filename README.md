2adf825aab8e6972dec493f340b57a515185fcab
ljaniec <janiec.lukas@gmail.com>
2022-09-06 16:05:35 +0200

    Example code with Python module from C++ code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pybind11-cpp-example
+
 Example C++ project with pybind11
+
+Links:
+
+- <https://www.youtube.com/watch?v=_5T70cAXDJ0>
---
+++ build/CMakeFiles/module_name.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ljaniec/workspace_Arendai/pybind11-cpp-example/main.cpp" "/home/ljaniec/workspace_Arendai/pybind11-cpp-example/build/CMakeFiles/module_name.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "module_name_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../pybind11/include"
+  "/usr/include/python3.8"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
